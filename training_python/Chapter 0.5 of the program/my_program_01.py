cb81f5f275b0e92f467a6b8dc6a6f3f0d059f5b9
Platon <platon.pydovzhenko@gmail.com>
2026-02-07 22:16:30 +0300

    Add files via upload
    In this commit, I added the completed exercise 5.1 "Checking conditions" and started 5.2, and also added to the experiments all sorts of programs with if that neural networks made for me, and I need this to understand how things work.

---
+++ training_python/Chapter 0.5 of the program/my_program_01.py
@@ -0,0 +1,6 @@
+#data in numbers
+dn = list(range(3, 336, 3))
+for n in dn:
+    print(n)
+print(max(dn))
+print(min(dn))
---
+++ training_python/Chapter 5/5.1-5.2.py
@@ -0,0 +1,50 @@
+#5.1
+t = 'titan'
+b = 50
+r = 'learnPython'
+n = 'ninjutsu'
+wine = 'red'
+
+#Let's check if this is titan.
+print("t == titan.I predict true")
+print(t == 'titan')
+#We check whether b is greater than or equal to 60.
+print("\nb >= 60.I predict false")
+print(b >= 60)
+#Let's check if it's amaterasu or ninjutsu.
+print("\nn == amaterasu or ninjutsu.I predict true")
+print(n == 'amaterasu' or 'ninjutsu')
+#We're checking white wine.
+print("\nwine == white.I predict false")
+print(wine == 'white')
+#Let's check that r is not Python.
+print("\nr != python.I predict true")
+print(r != 'python')
+#check that b is equal to 55
+print("\nb == 55.I predict false")
+print(b == 55)
+#check that r is learnPython
+print("\nr == learnPython.I predict true")
+print(r == 'learnPython')
+#check that the wine is pink
+print("\nwine == pink.I predict false")
+print(wine == 'pink')
+#Let's check that n is not genjutsu
+print("\nn != genjutsu.I predict true")
+print(n != 'genjutsu')
+##Let's check if this is TITAN
+print("\nt == TITAN.I predict false")
+print(t == 'TITAN') #<-----python is sensitive to lowercase and uppercase letters
+print("--------------------------------------------------------")
+#5.2
+#Here I will use cycles because it is faster and it seems better to me.
+names = ['Amored titan', 'Ghoul', 'Pain', 'Obito', 'Yagami']
+numbers = [30, 53, 67, 944, 35]
+for name in names:
+    #1
+    print(name != Ghoul)
+    print(name == titan)
+    #2
+    names.lower()
+    #3
+    
